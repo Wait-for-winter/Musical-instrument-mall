5735bd295df1ca6f249c7741cb926d120e5bc944
fcq66 <14132608+fcq66@user.noreply.gitee.com>
2024-08-31 20:37:31 +0800

    aa

--- app.py
+++ app.py
@@ -61,6 +61,56 @@ class RegisterAPI(MethodView):
         # 如果用户名或密码缺失，返回错误
         return jsonify({'error': 'Invalid data'}), 400
 
+class LoginAPI(MethodView):
+    def post(self):
+        # 从请求中获取 JSON 数据
+        data = request.get_json()
+        phone = data.get('phone')
+        password = data.get('password')
+
+        # 连接到 MySQL 数据库
+        try:
+            connection = pymysql.connect(
+                host='localhost',  # 数据库地址
+                user='root',  # 数据库用户名
+                password='root',  # 数据库密码
+                db='database',  # 数据库名称
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor
+            )
+            print("数据库连接成功")
+        except Exception as e:
+            print(f"草了没连上: {e}")
+            return jsonify({'error': '草了没连上'}), 500
+
+        try:
+            with connection.cursor() as cursor:
+                # 检查用户名和密码是否匹配
+                sql = "SELECT * FROM user WHERE phone=%s AND password=%s"
+                cursor.execute(sql, (phone, password))
+                result = cursor.fetchone()
+
+                if result:
+                    print("登上去惹")
+                    # 如果凭据正确，返回状态 1 表示登录成功
+                    return jsonify({'status': 1}), 200
+                else:
+                    print("登不上诶")
+                    # 如果凭据不正确，返回状态 0 表示登录失败
+                    return jsonify({'status': 0}), 401
+        except Exception as e:
+            print(f"数据库出问题惹 : {e}")
+            return jsonify({'error': '数据库出问题惹'}), 500
+        finally:
+            connection.close()
+
+        # 如果用户名或密码缺失，返回错误
+        return jsonify({'error': 'Invalid data'}), 400
+
+# 登录
+login_view = LoginAPI.as_view('login_api')
+app.add_url_rule('/login', view_func=login_view, methods=['POST'])
+# 注册
 register_view = RegisterAPI.as_view('register_api')
 app.add_url_rule('/register', view_func=register_view, methods=['POST'])
 
